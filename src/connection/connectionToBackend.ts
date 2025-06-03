31ac214f0f5996f584e8f6c36dd1657069948cfb
jsmblog <jsmblog19@gmail.com>
2025-06-02 20:42:00 -0500

    [ADD] port

--- src/connection/connectionToBackend.ts
+++ src/connection/connectionToBackend.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const URL = 'http://localhost:3000/api'
+const URL = 'http://192.168.0.143:3000/api'
 const stored = sessionStorage.getItem('user')
 const user = stored ? JSON.parse(stored) : null
 const token = user?.token || ''
